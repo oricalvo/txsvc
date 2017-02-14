9098fecb0c45cf10963092496ba2a433b606708f
oricalvo <ori.calvo@gmail.com>
2017-02-14 02:01:20 +0200

    Adding txsvc-ng2

--- samples/angular2/app/app.module.ts
+++ samples/angular2/app/app.module.ts
@@ -4,12 +4,12 @@ import {AppComponent}  from './components/app.component';
 import {ClockComponent} from "./components/clock.component";
 import {ContactListComponent} from "./components/contactList.component";
 import {ContactDetailsComponent} from "./components/contactDetails.component";
-import {RootStore, AppState} from "./stores/root.store";
+import {RootStore} from "./stores/root.store";
 import {ContactsStore} from "./stores/contacts.store";
 import {NewContactComponent} from "./components/newContact.component";
 import {FormsModule} from "@angular/forms";
 import {AuthStore} from "./stores/auth.store";
-import {AppStore} from "txsvc/AppStore";
+import {fromStores} from "txsvc-ng2/fromStores";
 
 @NgModule({
     imports: [
@@ -25,7 +25,7 @@ import {AppStore} from "txsvc/AppStore";
     ],
     bootstrap: [AppComponent],
     providers: [
-        AppStore.fromStores([
+        fromStores([
             RootStore,
             ContactsStore,
             AuthStore,
